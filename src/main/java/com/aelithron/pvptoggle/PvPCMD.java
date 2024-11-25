f3a0af52b1f3f1b6eefa9401937fc7bb656e8d7c
aelithron <aelithron@gmail.com>
2024-11-25 15:38:20 -0700

    Fix /pvp list This fixes the "/pvp list" command by changing the minimum arguments.
    Signed-off-by: aelithron <aelithron@gmail.com>

--- src/main/java/com/aelithron/pvptoggle/PvPCMD.java
+++ src/main/java/com/aelithron/pvptoggle/PvPCMD.java
@@ -19,26 +19,28 @@ public class PvPCMD implements CommandExecutor {
             return false;
         }
         Player player = (Player) sender;
-        if (args.length == 1 && (args[0].equalsIgnoreCase("t") || args[0].equalsIgnoreCase("toggle"))) {
-            if (CombatTimerManager.getInstance().checkStatus(player)) {
-                player.sendMessage(prefix + ChatColor.RED + "You cannot toggle PvP while in combat!");
-                return false;
-            }
-            ToggleManager.getInstance().togglePvP(player);
-            player.sendMessage(prefix + ChatColor.GREEN + "Your PvP status is now " + ChatColor.BOLD + (ToggleManager.getInstance().checkStatus(player) ? "on" : "off") + ChatColor.GREEN + ".");
-            return true;
-        }
-        if (args.length == 2 && (args[0].equalsIgnoreCase("l") || args[0].equalsIgnoreCase("list"))) {
-            sender.sendMessage(prefix + ChatColor.DARK_AQUA + "PvP Status List");
-            for (Player players : plugin.getServer().getOnlinePlayers()) {
-                boolean status = ToggleManager.getInstance().checkStatus(player);
-                if (status) {
-                    sender.sendMessage(ChatColor.GREEN + player.getName() + " • Enabled");
-                } else {
-                    sender.sendMessage(ChatColor.RED + player.getName() + " • Disabled");
+        if (args.length != 0) {
+            if (args[0].equalsIgnoreCase("t") || args[0].equalsIgnoreCase("toggle")) {
+                if (CombatTimerManager.getInstance().checkStatus(player)) {
+                    player.sendMessage(prefix + ChatColor.RED + "You cannot toggle PvP while in combat!");
+                    return false;
                 }
+                ToggleManager.getInstance().togglePvP(player);
+                player.sendMessage(prefix + ChatColor.GREEN + "Your PvP status is now " + ChatColor.BOLD + (ToggleManager.getInstance().checkStatus(player) ? "on" : "off") + ChatColor.GREEN + ".");
+                return true;
             }
-            return true;
+            if (args[0].equalsIgnoreCase("l") || args[0].equalsIgnoreCase("list")) {
+                sender.sendMessage(prefix + ChatColor.DARK_AQUA + "PvP Status List");
+                for (Player players : plugin.getServer().getOnlinePlayers()) {
+                    boolean status = ToggleManager.getInstance().checkStatus(player);
+                    if (status) {
+                        sender.sendMessage(ChatColor.GREEN + player.getName() + " • Enabled");
+                    } else {
+                        sender.sendMessage(ChatColor.RED + player.getName() + " • Disabled");
+                    }
+                }
+                return true;
+            }
         }
         player.sendMessage(prefix + ChatColor.GREEN + "Your PvP status is " + ChatColor.BOLD + (ToggleManager.getInstance().checkStatus(player) ? "on" : "off") + ChatColor.GREEN + ".");
         player.sendMessage(ChatColor.AQUA + "Use '/pvp t' to toggle your PvP status.");
